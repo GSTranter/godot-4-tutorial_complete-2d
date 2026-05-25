6ca06c5dd9779730559e80e4f81c2cbf62d7f526
George Tranter <georgetranter@hotmail.com>
2026-05-25 17:59:37 +0100

    Spawn rockets

--- projects/alien-attack/scripts/player.gd
+++ projects/alien-attack/scripts/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 var speed = 300
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer
+
 func _physics_process(delta):
 	velocity.x = _get_x_vel()
 	velocity.y = _get_y_vel()
 	move_and_slide()
+	check_shoot()
 	
 	global_position = global_position.clamp(Vector2(0, 0), get_viewport_rect().size)
 
@@ -19,3 +24,11 @@ func _get_y_vel():
 	if Input.is_action_pressed("move_up"): return -speed
 	if Input.is_action_pressed("move_down"): return speed
 	return 0
+
+func check_shoot():
+	if Input.is_action_just_pressed("shoot"):
+		var rocket_inst = rocket_scene.instantiate()
+		rocket_inst.global_position = global_position
+		rocket_inst.global_position.x += 80
+		
+		rocket_container.add_child(rocket_inst)
