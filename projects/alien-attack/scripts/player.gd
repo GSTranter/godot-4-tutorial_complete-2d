c919d143eceb9ed894efd9086fa73b615466c451
George Tranter <georgetranter@hotmail.com>
2026-05-25 17:17:19 +0100

    Clamp player position using builtins

--- projects/alien-attack/scripts/player.gd
+++ projects/alien-attack/scripts/player.gd
@@ -6,6 +6,9 @@ func _physics_process(delta):
 	velocity.x = _get_x_vel()
 	velocity.y = _get_y_vel()
 	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2(0, 0), get_viewport_rect().size)
+
 
 func _get_x_vel():
 	if Input.is_action_pressed("move_left"): return -speed
