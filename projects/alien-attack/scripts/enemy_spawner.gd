81f5742f6a5874fb6a1a52e9c112352655ed1b6e
George Tranter <georgetranter@hotmail.com>
2026-05-25 19:06:18 +0100

    Spawn enemies

--- projects/alien-attack/scripts/enemy_spawner.gd
+++ projects/alien-attack/scripts/enemy_spawner.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions.get_children()
+
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	spawn_enemy()
+
+func spawn_enemy():	
+	var enemy_inst = enemy_scene.instantiate()
+	enemy_inst.global_position = spawn_positions.pick_random().global_position
+	add_child(enemy_inst)
