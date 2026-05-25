1ee29a422efcb9484b0afe213fcfd6aa27108099
George Tranter <georgetranter@hotmail.com>
2026-05-25 16:33:41 +0100

    Player movement

--- projects/alien-attack/scripts/player.gd
+++ projects/alien-attack/scripts/player.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+var speed = 300
+
+func _physics_process(delta):
+	velocity.x = _get_x_vel()
+	velocity.y = _get_y_vel()
+	move_and_slide()
+
+func _get_x_vel():
+	if Input.is_action_pressed("move_left"): return -speed
+	if Input.is_action_pressed("move_right"): return speed
+	return 0
+	
+func _get_y_vel():
+	if Input.is_action_pressed("move_up"): return -speed
+	if Input.is_action_pressed("move_down"): return speed
+	return 0
