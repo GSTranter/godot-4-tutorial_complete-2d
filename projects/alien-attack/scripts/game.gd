57c900243d45d32929a74652bda8eab4f50557f6
George Tranter <georgetranter@hotmail.com>
2026-05-26 19:36:01 +0100

    Game over scene

--- projects/alien-attack/scripts/game.gd
+++ projects/alien-attack/scripts/game.gd
@@ -3,6 +3,8 @@ extends Node2D
 var lives = 3
 var score = 0
 
+var game_over_scene = preload("res://scenes/game_over_screen.tscn")
+
 @onready var player = $Player
 @onready var hud = $UI/HUD
 
@@ -10,13 +12,19 @@ func _ready():
 	hud.set_score_label(score)
 
 func _on_deathzone_area_entered(area: Area2D) -> void:
-	area.die()
+	area.queue_free()
 
 func _on_player_took_damage() -> void:
 	lives -= 1
 	hud.set_lives_label(lives)
 	if lives <= 0:
 		player.die()
+		
+		await get_tree().create_timer(1.5).timeout
+		var game_over_inst = game_over_scene.instantiate()
+		game_over_inst.set_score_label(score)
+		$UI.add_child(game_over_inst)
+		
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
 	add_child(enemy_instance)
